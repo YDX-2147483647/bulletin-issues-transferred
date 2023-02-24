98c17545569e82614ef42f8c5ce3fea09d1e2313
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-02-24 11:30:22 +0800

    [examples] Add RSS hook for servers
    Update `config.yml`

--- src/examples/server-cli.ts
+++ src/examples/server-cli.ts
@@ -6,11 +6,13 @@ import { hook, update_notices } from '../core/index.js'
 import { add_hook } from '../plugin/cli/index.js'
 import robot from '../plugin/ding/index.js'
 import add_proxy_hook from '../plugin/proxy/index.js'
+import add_rss_hook from '../plugin/rss/index.js'
 import { logger } from '../util/logger.js'
 
 add_hook.verbose(hook)
 add_hook.progress_bar(hook)
 add_proxy_hook(hook)
+add_rss_hook(hook)
 
 const { new_notices, change } = await update_notices()
 
