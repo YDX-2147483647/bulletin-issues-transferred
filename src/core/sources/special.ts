7ec80055336b22a2ed7cac98ec3b4fd8adf7c2ca
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-05-09 13:37:53 +0800

    [config] sources 添加“图书馆”
    Relates-to: #23

--- src/core/sources/special.ts
+++ src/core/sources/special.ts
@@ -77,6 +77,56 @@ const raw_sources: SourceSpecialInterface[] = [
             )
         },
     },
+    {
+        name: '图书馆',
+        guide: [
+            '图书馆',
+            '动态发布',
+            '通知公告',
+            '更多',
+        ],
+        url: 'https://lib.bit.edu.cn/engine2/m/7252A82F2C1BEA45',
+        async fetch_notice ({ _hook }) {
+            const response =
+                await hooked_fetch({
+                    url: 'https://lib.bit.edu.cn/engine2/general/1430281/type/more-datas',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    },
+                    body: 'engineInstanceId=2047535',
+                    _hook,
+                })
+            const json = await response.json() as {
+                data: { datas: { datas: { url: string, '1': { value: string }, '6': { value: string }, id: string }[] } }
+            }
+            const original_data = json.data.datas.datas
+
+            return original_data.map(
+                ({ url: url_str, 1: title, 6: date, id }) => {
+                    const url = new URL(url_str, 'https://lib.bit.edu.cn')
+                    if (url.hostname === 'lib.bit.edu.cn') {
+                        // 图书馆直接返回的是长 URL，
+                        // 实际访问时再用`window.location.href`跳转到实际页面。
+                        // 这里直接缩短一下。
+                        url.pathname = [
+                            'engine2/d',
+                            id,
+                            url.searchParams.get('engineInstanceId'),
+                            '0',
+                        ].join('/')
+                        url.search = ''
+                    }
+
+                    return {
+                        link: url.href,
+                        title: title.value,
+                        date: parse_date(date.value),
+                    }
+                },
+            )
+        },
+    },
 ]
 
 const sources = raw_sources.map(raw => {
