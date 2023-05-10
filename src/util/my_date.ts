45771a90360cf996b71f87ea14d9930f13ed4915
Y.D.X <73375426+YDX-2147483647@users.noreply.github.com>
2023-05-10 13:30:13 +0800

    feat: Parse date “2023-03-31 17:28:49”

--- src/util/my_date.ts
+++ src/util/my_date.ts
@@ -4,7 +4,7 @@
  * @description 会忽略日期两边的字符。未标时区时采用本地时间。
  */
 export function parse_date (date: string) {
-    const match = date.match(/((?<year>\d+)[-/年])?(?<month>\d+)[-/月](?<day>\d+)日?((?<hour>\d+)时)?(?![-/T\d])/)
+    const match = date.match(/((?<year>\d+)[-/年])?(?<month>\d+)[-/月](?<day>\d+)日?((?<hour>\d+)时)?(?![-/T\s\d])/)
     if (match) {
         return new Date(parseInt(match.groups.year) || (new Date()).getFullYear(),
             parseInt(match.groups.month) - 1,
