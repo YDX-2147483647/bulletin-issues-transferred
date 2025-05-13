c8d29fd3bc06c41b34b15da37cc137b43cd8c923
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-05-13 23:23:57 +0800

    chore: 删除旧版统一身份认证

--- src/plugin/proxy/lib/encryptPassword.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @param password 原始密码
- * @param salt {@link Preparation}中的`salt`
- * @returns 加密后的密码
- *
- * 在原网站中，后端只接收加密后的密码，前端匿名，因此有这一步骤。
- */
-export function encryptPassword(password: string, salt: string): string
